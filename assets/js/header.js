6c684d6bfbf41c4626a12a0cc31ccbc835848c7f
netpork <netpork@gmail.com>
2014-08-27 13:23:37 +0200

    stats

--- assets/js/header.js
+++ assets/js/header.js
@@ -19,12 +19,26 @@ Teleportal.header = function() {
 
 	function insertPage(page) {
 		Teleportal.getContext().render(Teleportal.getTemplatesPath() + '/pages/' + page + '.ms')
-		.appendTo(Teleportal.getMainContainer());
+		.appendTo(Teleportal.getMainContainer())
+		.then(function() {
+			if (page === 'domov') {
+				makeBars();
+			}
+		});
 	}
 
 	function setActive(idx) {
 		var tabs = $('#navigation').find('li');
 		$(tabs[idx]).addClass('active');
+	}
+
+	function makeBars() {
+		$("#stats-registered").sparkline(
+			[5, 10, 11, 12, 15, 25,36,37,33,34,40,50,41],
+			{
+    			type: 'bar',
+    			height: '16'
+    		});
 	}
 
 	return {
