81f57e43aedd3d0c829e2bb9c9409a3165205068
netpork <netpork@gmail.com>
2014-09-08 14:43:41 +0200

    fixes

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -6,16 +6,15 @@ Teleportal.forum = function() {
 	var topics = {
 		'anxiety': [
 			{
-				'title': 'Anxiety back trying to taper off Xanax with Lyricia',
-				'question': "Hi, I'm new here. Just wondering if anyone has ever got off of Xanax compleatly. I've been put on Lyricia to help with this but my anxiety seems to be worse in the last few days only started decreasing Xanax by .25mg a day and started 25mg of Lyricia Friday. Not sure if it's the new tab or just cutting down the Xanax. I've been on Xanax for over 15 years different doses I'm on .5mg 3times a day have just cut that by .25mg a day. Wondering if anyone ever got off these tablets. Any suggestions would be great. Thanks",
-				'author': 'crazyfrog',
-				'answer1_title': 'rosemary » crazyfrog',
-				'answer1_text': "be very careful with pregabalin/lyrica, I only took 28x25mg capsules over 21 days,  and I am addicted to it, I have just about  every side effect  listed, plus lots not listed,  from day one I knew from the first cap that I didnt want to take it because side effects were instant, but the attitude was 'take it or else'  I have been trying to withdraw it since july. I feel very ill and it has chaged me completely, I am alter ego, I am like drunk andy pandy,  my legs are weak,wobbly, I stagger like a drunk, I am using liquid with a syringe to cut down with a smaller dose, but even this is hit and miss, today has been awful, as are most days,  I have pain in belly,like a hot sensation, someone pulling and squeezing,  I just want it all to be over..  some will say it's ok for them, but for me it has made me very ill, pain , which I didn't have before is excruciating, not relieved by the medication, if in any doubt about whether to use it , could you cope with what I have just described ? . take care, bye,rosie.",
-				'answer1_author': 'rosemary',
-				'answer2_title': 'crazyfrog » rosemary',
-				'answer2_text': "Thanks for taking time to reply. I find it hard enough to carry on the way I am for the last few days Idon't want to become addicted to this as well they sound awful. I thought the would help as I reduced my dosage of Xanax which I've been on for more than 15 yrs. I want to get off them as they are not doing anything for me but I know I can't go cold tukey it's too dangerous. I've loads of different meds tried over the years for anxiety,panic,agoraphobia PTSD and depression. I'm in therapy and attend a support group once a week. Doc wanted me to try seroquil (you heard of it?)a few months ago I didn't even try them. Just don't know what to do anymore the anxiety is high at the moment it seems to be there all day until i finally get to sleep.will go to doc again tomorrow see what he says.I hope you withdraw from it safely and you feel better soon best of luck and thanks for your advice.",
-				'answer2_author': 'crazyfrog',
-
+				'title': 'Notrnaji mir',
+				'question': "Pozdravljeni! Padel sem na dno in ne vem več kaj naj naredim zato sem se obrnil na ta forum, da vsaj dam iz sebe vsaj nekaj stvari, ki me uničujejo. Torej za kaj se gre. Star sem 32 let. Od 26.ga do 30.ga leta sem živel na svojem v stanovanju za katerega sem plačeval kredit in je bilo moje. Oče je sicer vzel kredit, ker jaz več ali manj že od 19-ga leta delam preko avtoske napotnice in nisem bil nikoli rendo zaposlen. ( ukvarjam se bol z umetniško dejavnostjo in v Sloveniji težko dobim redno zaposlitev na svojem področju ) sem pa prevezel seveda odplačevanje kreditov. No do 29.ga leta je šlo..imel sem podporo staršev in služil zase več kot povprečni slovenec. Pred dvema letoma mi je umrl oče in tkarat so se začeli problemi. Za sabo je pustil nekaj kreditov, jaz pa sem zaradi goljufije prodajalca in debilne države izgubil stanovanje za nedoločen čas. Vrnil sem se nazaj domov k mami, ki pa je invalidsko upokojena za polovični delvoni čas. V teh dveh letih sem res trpel in poplačal vse kredite...sicer z mamino pomočjo a kot pravim ima tkao majhne prihodke da sem večino jaz prispeval. Kar pa pomeni, da že zadnje 3 leta od tega dogodka, živim praktično skoraj na robu revščine. Sicer mi je ponavadi ostalo, da sem imel za bencin i hrano in tobak a vse ostalo je izginilo. Naj omenim še da sem v 4 letni zvezi s punco, ki je mlajša od mene 9 let.",
+				'author': 'Živčni',
+				'answer1_title': 'sabi1234 » Živčni',
+				'answer1_text': "Na tvojem mestu bi za začetek pustila punco, tako bi zaprla neka stara vrata, ki me vlečejo nazaj in odprla nova.To, da ti ni stala ob strani pri izgubi očeta se mi zdi grozno.Ni opravičilo, da je mlajša.Tudi jaz sem veliko mlajša od svojega fanta, ampak očitno veliko bolj zrela od tvoje punce.Če si preživel izgubo ljubega očeta, boš tudi izgubo težavne punce.Po vajinem razhodu bo pol težav manj, ti pa se lahko resno osredotočaš na druge probleme in si čez čas najdeš punco, ki ti bo pripravljena stati ob strani.Preživel si zelo težko obdobje, ampak dejstvo je da mora človek, ki želi spremembe tudi dejansko nekaj zaključiti/spremeniti.In na žalost v tvoji zgodbi tvoje punce ne dojejmam kot pozitivnega lika.Tudi mislim, da si preveč čustven tip...praviš svojo punco imam tako rad, da je ne bi izgubil.Tako rad, čeprav si ni najdla službe, te ni razumela ko si izgubil očeta in te je varala? Imam občutek, da si človek ki si dovoli, da hodijo po njemu.Ko pridobiš nekaj denarja je prvo kar naredi znjim, da ga vloži v potovanje na katerega pojdi sam.Na tvojem mestu bi odšla v kambodžo,indijo,tajsko (zelo poceni države,z iskrenimi ljudmi in veliko manj plehkimi ljudmi) Denar ti resnično preveč pomeni, pa veš zakaj, ker te ljudje iskoriščajo.Ker veš, da brez njega ne moreš osrečiti svoje punce, kako žalostno.Jaz in moj fant sva bila resnično srečna v obdobju ko sva bila brez ficka, ampak imea sva en drugega.Tvoj oče bi bil ponosen nate, če bi videl da si srečen, ne pa če bi videl, da imaš veliko denarja.Spremeniti moraš svoj pogled na svet in to nujno.Nekateri ljudje so srečni pa čeprav jejo 3x na teden zelje in krompir, nekateri zato rabijo 3x na teden obisk restavracije....Znebil se boš negativnih čustev, ko se boš znebil negativnih ljudi.Pa še nekaj praviš da si star 32 let in da bi rad imel počasi družino.Glede na to, da si moški mislim, da ti ne tik taka biološka ura.Hočem ti povedati, da če pustiš svojo punco,si urediš življenje, imaš prav tako lahko srečno družinico čez par let.Ne vidim, kakšna bi bila razlika če je tvoja sedanja punca stara 23 let, dvomim da bi imela otroka stabo ki te ima na 'opazovanju'. Nujno si moraš zgraditi svojo samopodobo, beri knjige, obudi svoja stara prijatelstva, ukvarjaj se s športom (zastojn šport, ki ti napolni duha: tek v naravi) in kot sem že velikokrat poudarila pusti svojo punco.Nekaj ti polagam na srce: DENAR NI SREČA,PRAVA SREČA JE ISKRENA LJUBEZEN. p.s:upam, da nisem bila prestroga, ampak resnično ti želim da se najdeš. Lp, tvoja mom prijateljica :)",
+				'answer1_author': 'sabi1234',
+				'answer2_title': 'markookram » Živčni',
+				'answer2_text': "Glede službe in financ ti ne morem svetovati, pač glavo pokonci in prebrodi krizo. Saj mogoče nebo zadnja, življenje ima vzpone in padce. AMPAK glede te tvoje 'punce' pa je več kot očitno, da je samo pijavka in NI ta tvoji strani. Še enkrat preberi kaj si napisal in kdaj je ona tebe podpirala? Kdaj je pokazala da je tvoja in da si ti njen? Ne stoji ti ob strani. Skupaj v dobrem, če ne pa skoči k drugemu. Mogoče te nebi mogla podpirati finančno (tudi tukaj ni izgovora bi si našla delo, karkoli če je kriza) ampak niti moralno in čustveno te ni podprla jaz bi se počutil skoraj izdanega. Še huje prevarala te je, to pa je izdajalstvo. Bi skoraj rekel super da si se finančno zlomil in sedaj vidiš kdo je bila ta tvoja življenska sopotnica. Zapomni si kdo ti je stal ob strani ko si bil na tleh! Vse ostale briši iz življenja! Tako da glavo pokonci in zruši tiste zidove pred tabo in jih zgradi za sabo. Nikoli ne obupaj, daj 100% od sebe, da vsaj bolečine obžalovanja ne boš občutil. Nisi edini, življenje je borba.",
+				'answer2_author': 'markookram',
 				'views': function() {
 					return function() {
 						return ' ' + Teleportal.forum.randValue();
@@ -27,7 +26,7 @@ Teleportal.forum = function() {
 			},
 
 			{
-				'title': 'Health anxiety!',
+				'title': 'Nova knjiga v slovenščini - Mejna osebnostna motnja za telebane',
 				'question': "Hi, newbie here looking for... well to be honest I don't know whether it's answers, reassurance, or just a friendly opinion I'm looking for, but all are welcome. I'm a man in my 30s and for the past few years have been suffering from anxiety. Before the anxiety I was confident, enjoying work, had more friends than I had time to spend with, and was earning good money. Everything revolved around my social life. It was the reason I worked, and all I thought about during the day - what I could get up to at the weekend. Stress and anxiety was something that other people suffered from, but not me. I wasn't interested in settling down, not unusual for a guy in his 20s. Then completely out of the blue, I met a girl. A wonderful, gorgeous, kind, and funny girl, and immediately this changed me. She was the one. In case anyone assumes this is a story about how this girl broke my heart, this is not the case. We are still together, very much in love, engaged, and have a beautiful 2 year old son.",
 				'author': 'jasmine',
 				'views': function() {
@@ -41,7 +40,7 @@ Teleportal.forum = function() {
 			},
 
 			{
-				'title': 'Chronic anxious insomnia - will citalopram help eventually?',
+				'title': 'moževi izbruhi',
 				'question': "Hi there! I was on citalopram years ago for anxiety and it really helped, I was on it for a long time. Lately I started feeling anxious again, which has now heightened to severe at night time - when I go to bed it feels like my heart might explode in my chest. There is just no way of relaxing, except when I take 1 zopiclone. I don't want to take these forever I know they're addictive, but I'm wondering if anyone has had this experience? I'm hoping once the citalopram kicks back in I'll feel strong and if I don't sleep I'll be able to cope, just at the moment feeling anxious in bed all night sets me up for a terrible day the next day. I used to sleep fine when I was on citalopram (20mg and even 10mg) but I didn't really have sleeping problems before then. Can anyone help?",
 				'author': 'ahmad',
 				'views': function() {
@@ -55,7 +54,7 @@ Teleportal.forum = function() {
 			},
 
 			{
-				'title': 'Help - Anxiety Chest Pain or acid reflux?',
+				'title': 'osamljenost',
 				'question': "Hi guys, I think I’m going crazy. I have had chest pains on the left side of my chest for the last couple of months now. I have it on and off every day. It’s either right in the middle at the bottom of my rib cage or up at the top below the first rib. I have had a couple of ECG’s, xray, echocardiogram, and various blood tests. Everything has come back normal but as soon as I have the pain I go back to thinking there is problem. The doctors have said its anxiety related and 7 weeks ago put me on 40mg of citalopram. The citalopram has really helped my worrying but it’s not taken away the pain. A couple of people have said that it could be due to acid reflux but can acid reflux give you a pain that radiates all over the left side of my chest and arm? I feel slighty lost at the moment to be honest and I just want to enjoy life. Andy",
 				'author': 'littleNarruto',
 				'views': function() {
@@ -69,7 +68,7 @@ Teleportal.forum = function() {
 			},
 		
 			{
-				'title': 'how much physical pain can anxiety actually cause?',
+				'title': 'Komunikacija z mamo in pretiran strah',
 				'question': "I haven't posted for a while, but basically for some time I had been suffering with severe chest pains, initially the doctors thought I had a hiatus hernia, I was treated with esomeperazole, I also had a gastroscopy but this came back clear. I then had some scans and xrays and it shew two very small kidney stones, I was then told I had a form of ibs, ive been switched from med to med. I took myself to hospital one night as I was convinced I was dying from heart issues, I had ecgs and xrays and blood tests and all came back ok but still suffering with pain, my latest trip to the doctors I got told I had costochondritis, now been put on naproxen but still no ease from the pain. Ijust wondered if anxiety is able to cause such severe constant pain? I do worry a lot which in turn makes the pain worse which makes the anxiety worse and so on...please can anyone help? X",
 				'author': 'eski',
 				'views': function() {
@@ -83,7 +82,7 @@ Teleportal.forum = function() {
 			},
 
 			{
-				'title': 'winter blues',
+				'title': 'nikomur ne zaupam',
 				'question': "i hate earlier darker nights as winter approcahes . Does anyone else get pannicky ?",
 				'author': 'nil',
 				'views': function() {
